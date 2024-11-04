0dcd7e85274f5062be519e8a6eccc0a82c4705cf
Alyson Lima <alysonrafael2005@gmail.com>
2024-11-04 02:03:16 +0000

    Questão 2, ordenação em ordem crescente(InsertionSort)

--- ordenação-vetores/sort.java
+++ ordenação-vetores/sort.java
@@ -8,6 +8,13 @@ public class sort {
         
         selectionSort(vetor);
         System.out.println("Vetor ordenado em ordem crescente: " + Arrays.toString(vetor));
+        
+        // Questão 2 - Insertion Sort em ordem crescente
+        int[] vetor2 = {34, 8, 64, 51, 32, 21, 4, 12, 67, 19, 78, 45, 34, 88, 29, 57, 83, 92, 6, 11};
+        System.out.println("\nQuestão 2 - Insertion Sort em ordem crescente");
+        System.out.println("Vetor original: " + Arrays.toString(vetor2));
+        insertionSort(vetor2);
+        System.out.println("Vetor ordenado em ordem crescente: " + Arrays.toString(vetor2));
     }
     public static void selectionSort(int[] vetor2) {
         int x = vetor2.length;
@@ -23,6 +30,19 @@ public class sort {
                 vetor2[i] = vetor2[indiceMenor];
                 vetor2[indiceMenor] = temp;
             }
+        }
+    }
+     // Questão 2 - Insertion Sort em ordem crescente
+     public static void insertionSort(int[] vetor) {
+        int n = vetor.length;
+        for (int i = 1; i < n; i++) {
+            int chave = vetor[i];
+            int j = i - 1;
+            while (j >= 0 && vetor[j] > chave) {
+                vetor[j + 1] = vetor[j];
+                j = j - 1;
+            }
+            vetor[j + 1] = chave;
         }
     }
 }
